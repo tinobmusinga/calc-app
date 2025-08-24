3993eb4b54f0049dc43cba1df657625391a39520
Tinotenda Musingarimi <tinobmusinga@gmail.com>
2025-08-25 01:19:03 +0200

    Initialize node server server

--- back-end/server.js
+++ back-end/server.js
@@ -1,1 +1,13 @@
-console.log("This my backend App");
+// back-end/server.js
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.get("/", (req, res) => {
+  res.send("Hello from Cloud Run!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
